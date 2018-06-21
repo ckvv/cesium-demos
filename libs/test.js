fb157136ae221c552f4522cf136312e4d6ca604f
陈锴 <2351988538@qq.com>
2018-06-21 16:31:38 +0800

    Delete test.js

--- libs/test.js
+++
@@ -1,7 +0,0 @@
-// 定义当前场景的画布元素的事件处理
-var handler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
-function t1 () { 
-    console.log('t1');
- };
-//设置鼠标移动事件的处理函数，这里负责监听x,y坐标值变化
-handler.setInputAction(t1, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
